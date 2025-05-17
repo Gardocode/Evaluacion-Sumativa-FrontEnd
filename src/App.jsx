26ef36d1f2f4088d243e2fecbb7ae78b0cb7ac9e
Gardo <nixito_16@hotmail.com>
2025-05-16 21:40:02 -0400

    Commit inicial para la estructura del proyecto y configurar react router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+
+const router = createBrowserRouter([
+  {path: "/", element: <Home/>},
+  {path: "/checkout", element: <Checkout/>}
+]);
+
+function App() {
+  return <RouterProvider router={router}/>;
+}
+
+export default App
